a63c3501aa86a9070ba7e1d93675f2828ccf3a1f
BrunoB <tommyboy3816@yahoo.com>
2023-01-07 13:57:48 -0600

    final release of first revision

---
+++ main.py
@@ -0,0 +1,53 @@
+import os
+from glob import glob
+import time
+import logging
+import music_tag
+import string
+import csv
+
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def find_mp3s(dir):
+    num_mp3s = 0
+
+    out = csv.writer(open("d:\low_quality_mp3s.csv", "w", newline=''), delimiter="%", quoting=csv.QUOTE_NONE, escapechar='\\')
+    out.writerow(["artist", "title", "album", "bitrate", "codec", "length", "sample rate"])
+
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Looking for mp3s in {dir}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+    result = [y for x in os.walk(dir) for y in glob(os.path.join(x[0], '*.mp3'))]
+    num_mp3s = len(result)
+
+    num_mp3s = 0
+
+    for name in result:
+        if name.endswith(".mp3"):
+            num_mp3s+=1
+            # print("{:5d}) {}".format(num_mp3s, name))
+            f = music_tag.load_file(name)
+            title = f['title']
+            artist = f['artist']
+            bitrate = int(f['#bitrate'])/1000
+            codec = f['#codec']
+            length = int(f['#length'])
+            sr = int(f['#samplerate'])
+            album = f['album']
+            #print("   {}, {}, {}, {}, {}, {}".format(artist, title,bitrate/1000, codec, length, sr))
+            if bitrate < 256:
+                print("%5d) %-*s %-*s %-*s %3d %-*s %5d" % (num_mp3s, 25, artist, 40, title, 20, album, bitrate, 6, codec, length))
+                info = [artist, title, album, bitrate, codec, length, sr]
+                # print(info)
+                out.writerow(info)
+
+    return(num_mp3s)
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    num_mp3s = find_mp3s('M:\\')
+    print("Found {} mp3s".format(num_mp3s))
+
