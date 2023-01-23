4340f1aba9c3b49d761f8efa9764045fe430e3f0
Luke <tommyboy3816@yahoo.com>
2023-01-22 18:25:03 -0600

    - writing with a different spreadsheet library  - find/replace 'Various Artists' album artist with actual artist  - command line parameters to find low quality mp3s or find files with 'Various Artists'

--- main.py
+++ main.py
@@ -2,44 +2,58 @@ import os
 from glob import glob
 import music_tag
 import csv
-import xlwt
 import time
-
+import sys
+import argparse
+import xlsxwriter
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def find_mp3s(mp3_dir):
-    num_mp3s = 0
-    sheet = "low_qual_mp3s"
-    filename = "D:\\low_quality_mp3s"
+def find_mp3s(mp3_dir, is_low_bitrate, is_various):
 
-    book = xlwt.Workbook()
-    sh = book.add_sheet(sheet)
-    currow = 0
-    curcol = 0
-    column_hdrs = ["artist", "title", "album", "bitrate", "codec", "length", "sample rate", "location"]
+    if is_low_bitrate:
+        sheet = "low_qual_mp3s"
+        filename = "D:\\low_quality_mp3s"
+    elif is_various:
+        sheet = "various_artists_mp3s"
+        filename = "D:\\various_artists_mp3s"
+    else:
+        print("INVALID CONFIG!!! is_low_bitrate {}, is_various {}".format(is_low_bitrate, is_various))
+        return -1
+
+    wb = xlsxwriter.Workbook(filename + '.xlsx')
+    ws = wb.add_worksheet('low_qual_mp3s')
+
+    row = 0
+    col = 0
+    column_headers = ["artist", "title", "album", "album_artitst", "bitrate", "codec", "length", "sample rate", "location"]
+    column_widths = [32, 32, 32, 32, 7, 7, 7, 7, 48]
+    column_align = [{'align': 'left'}, {'align': 'left'}, {'align': 'left'}, {'align': 'left'}, {'align': 'center'}, {'align': 'center'}, {'align': 'center'}, {'align': 'center'}, {'align': 'left'} ]
 
     out = csv.writer(open(filename + ".csv", "w", newline=''), delimiter="%", quoting=csv.QUOTE_NONE, escapechar='\\')
-    out.writerow( column_hdrs )
-    # sh.write( column_hdrs )
-    c0 = "artist"
-    sh.write(currow, 0, c0)
-    sh.write(currow, 1, "title")
-    sh.write(currow, 2, "album")
-    sh.write(currow, 3, "bitrate")
-    sh.write(currow, 4, "codec")
-    sh.write(currow, 5, "length")
-    sh.write(currow, 6, "sample rate")
-    sh.write(currow, 7, "location")
-    currow+=1
+    out.writerow( column_headers )
+
+    for hdr in column_headers:
+        print("{} of {}) colhdr {} ({})".format(col+1, len(column_headers), hdr, column_align[col]))
+        ws.write(row, col, hdr)
+        center = wb.add_format(column_align[col])
+        ws.set_column(col, col, column_widths[col], center)  # Set column width
+        col += 1
+
+    # Adding autofilters to the column headers
+    ws.autofilter( row, 0, row, len(column_headers)-1 )
+
+    row += 1
+    col = 0
 
     # Use a breakpoint in the code line below to debug your script.
     print(f'Looking for mp3s in {mp3_dir}')  # Press Ctrl+F8 to toggle the breakpoint.
 
     result = [y for x in os.walk(mp3_dir) for y in glob(os.path.join(x[0], '*.mp3'))]
     num_mp3s = len(result)
+    print( num_mp3s )
 
     num_mp3s = 0
 
@@ -48,38 +62,58 @@ def find_mp3s(mp3_dir):
             num_mp3s+=1
             # print("{:5d}) {}".format(num_mp3s, name))
             f = music_tag.load_file(name)
+
             artist = str(f['artist'])
             title = str(f['title'])
             album = str(f['album'])
+            album_artist = str(f['album artist'])
             bitrate = int(f['#bitrate'])/1000
             codec = str(f['#codec'])
             length = int(f['#length'])
             sr = int(f['#samplerate'])
             # print("   {}, {}, {}, {}, {}, {}".format(artist, title,bitrate/1000, codec, length, sr))
-            if bitrate < 256:
-                print("%5d) %-*s %-*s %-*s %3d %-*s %5d, %-*s" % (num_mp3s, 25, artist, 40, title, 20, album, bitrate, 6, codec, length, 64, name))
-                info = [artist, title, album, bitrate, codec, length, sr, name]
-                # print(info)
-                out.writerow(info)
-                sh.write(currow, 0, artist)
-                sh.write(currow, 1, title)
-                sh.write(currow, 2, album)
-                sh.write(currow, 3, bitrate)
-                sh.write(currow, 4, codec)
-                sh.write(currow, 5, length)
-                sh.write(currow, 6, sr)
-                sh.write(currow, 7, name)
-                currow+=1
+            if ((True == is_low_bitrate) and (bitrate < 256)) or \
+                    ((True == is_various) and ((album_artist == 'Various') or (album_artist == 'Various Artists'))):
+                print("%5d) %-*s %-*s %-*s %-*s %3d %-*s %5d, %-*s" %
+                      (num_mp3s, 25, artist, 40, title, 20, album, 25, album_artist, bitrate, 6, codec, length, 64, name))
+                # f['album_artist'] = artist
+                # f.save()
+                info = [artist, title, album, album_artist, bitrate, codec, length, sr, name]
 
-    book.save(filename + ".xls")
+                for item in info:
+                    ws.write(row, col, item)
+                    col += 1
 
-    return(num_mp3s)
+                row += 1
+                col = 0
+
+    wb.close()
+
+    return num_mp3s
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # music_dir = 'M:\\'
-    music_dir = 'E:\\Music'
+    music_dir = 'M:\\'
+    # music_dir = 'E:\\Music'
+
+    # Remove 1st argument from the
+    # list of command line arguments
+    argumentList = sys.argv[1:]
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--directory')
+    parser.add_argument('-v', '--various', action='store_true')
+    parser.add_argument('-l', '--low_bitrate', action='store_true')
+    # parser.add_argument('-h', '--help')
+    args = parser.parse_args()
+    print(args.directory, args.various, args.low_bitrate)
+
+    if args.directory:
+        music_dir = args.directory
+    else:
+        print("no directory defined, using {}".format(music_dir))
+
     start_time = time.time()
-    number_of_mp3s = find_mp3s( music_dir )
+    number_of_mp3s = find_mp3s( music_dir, args.low_bitrate, args.various )
     print("Found {} mp3s at {} in {:4.2f} seconds".format(number_of_mp3s, music_dir, (time.time() - start_time)))
 
